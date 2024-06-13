1782bcbb535f9a1b54aa3e7e1c4110bb60db9b55
unknown <astutecollins4law@yahoo.com>
2024-06-13 17:21:19 +0100

    fixed table.tsx[lusitana] font import from the font.tsx file

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,6 +1,6 @@
 import {Inter, Lusitana} from 'next/font/google'
 export const inter = Inter({subsets: ['latin']});
-export const lusi = Lusitana({subsets: ['latin'], 
+export const lusitana = Lusitana({subsets: ['latin'], 
 variable: '--font-lusi', 
 display: 'swap',
 weight: ['400', '700'],
